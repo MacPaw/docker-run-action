04c5b21639103cf1ced250c7e7101be5dfac6943
Serhii Donii <serhii.donii@macpaw.com>
2026-02-17 12:01:33 +0200

    feat: update entrypoint run command

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,6 @@ if [ ! -z $INPUT_DOCKER_NETWORK ];
 then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
 fi
 
-exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
+echo $INPUT_RUN > /tmp/run.sh
+
+exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE /tmp/run.sh
